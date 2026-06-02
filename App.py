d25c7fe9394aa1f4740b8bb3fcf5454b6950890d
Chiragjsca <chiragjsca@gmail.com>
2026-06-03 00:36:02 +0530

    Update App.py

--- App.py
+++ App.py
@@ -19,7 +19,7 @@ st.set_page_config(page_title="Top 250 NSE Stock-Turnover Breakout Dashboard", l
 # ==========================================
 # 🔐 ADMIN LOGIN SYSTEM
 # ==========================================
-ADMIN_PASSWORD = "admin"
+ADMIN_PASSWORD = "dada"
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
